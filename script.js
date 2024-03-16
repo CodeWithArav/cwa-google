e68712327a354f88a4d373f9f0a827bb3433f2c9
CodeWithArav <121603770+CodeWithArav@users.noreply.github.com>
2024-03-16 18:18:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Variables //
+
+let cross_btn = document.querySelector(".cross-icon");
+let input_box = document.querySelector(".search-box");
+
+// Clear Search Bar //
+
+function clearText() {
+    input_box.value = ``;
+}
